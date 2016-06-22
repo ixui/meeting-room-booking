80b8ec0ef88cf7ab9f377ad051a57d7556739347
tamura-shunsuke <f09h0224@yahoo.co.jp>
2016-06-22 09:25:58 +0900

    修正

--- MeetingRoomBookingApp/src/main/java/jp/co/ixui/tamura/domain/Reservation.java
+++ MeetingRoomBookingApp/src/main/java/jp/co/ixui/tamura/domain/Reservation.java
@@ -1,14 +1,9 @@
 package jp.co.ixui.tamura.domain;
 
-<<<<<<< HEAD
-public class Reservation {
-
-=======
 /**
  * @author tamura
  *
  */
 public class Reservation {
->>>>>>> refs/remotes/origin/master
 	
 }
