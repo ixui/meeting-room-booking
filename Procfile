5e9261ee46141531937beeed9195b706e095a067
tamura-shunsuke <f09h0224@yahoo.co.jp>
2016-07-05 10:30:28 +0900

    mysqlに変更

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT -jar  target/MeetingRoomBookingApp-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT -jar -Dspring.profiles.active=heroku target/MeetingRoomBookingApp-0.0.1-SNAPSHOT.jar
--- src/main/java/jp/co/ixui/tamura/DatabaseConfig.java
+++ src/main/java/jp/co/ixui/tamura/DatabaseConfig.java
@@ -1,10 +1,12 @@
 package jp.co.ixui.tamura;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,14 +19,32 @@ import org.springframework.context.annotation.Configuration;
 public class DatabaseConfig {
 	@Autowired
 	DataSourceProperties properties;
-	DataSource dataSource;
 
-//	@Bean(destroyMethod="close")
-//	DataSource realDatasource() throws URISyntaxException {
-//		String url;
-//		String userName;
-//		String password;
-//
-//		String databaseUrl = System.getenv("DATABASE_URL");
-//	}
+	@Bean(destroyMethod="close")
+	DataSource datasource() throws URISyntaxException {
+		String url;
+		String userName;
+		String password;
+
+		String databaseUrl = System.getenv("DATABASE_URL");
+		if (null != databaseUrl) {
+			//heroku
+			URI dbUri = new URI(databaseUrl);
+			url = "jdbc:mysql://" + dbUri.getHost() + dbUri.getPath() + ":" + dbUri.getPort() + dbUri.getPath();
+			userName = dbUri.getUserInfo().split(":")[0];
+			password = dbUri.getUserInfo().split(":")[1];
+		} else {
+			//localhost
+			url = this.properties.getUrl();
+			userName = this.properties.getUsername();
+			password = this.properties.getPassword();
+		}
+
+		DataSourceBuilder factory = DataSourceBuilder
+				.create(this.properties.getClassLoader())
+				.url(url)
+				.username(userName)
+				.password(password);
+		return factory.build();
+	}
 }
