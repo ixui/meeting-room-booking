e0414129035c8d1f10dc4fbf7df8148c78e87b1d
t-kawasaki <t-kawasaki@yc1.jp>
2016-07-11 11:12:03 +0900

    接続設定を利用するように再変更 Flywayの導入が原因ではなさそう

--- src/main/java/jp/co/ixui/tamura/DatabaseConfig.java
+++ src/main/java/jp/co/ixui/tamura/DatabaseConfig.java
@@ -8,8 +8,10 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 /**
  * @author tamura
@@ -20,7 +22,9 @@ public class DatabaseConfig {
 	@Autowired
 	DataSourceProperties properties;
 
+	@Primary
 	@Bean(destroyMethod="close")
+	@ConfigurationProperties(prefix="spring.datasource")
 	DataSource datasource() throws URISyntaxException {
 		String url;
 		String userName;
