274a5f6bef2553f9d82341e93d8c200148517219
t-kawasaki <t-kawasaki@yc1.jp>
2016-07-12 15:36:23 +0900

    FluentLenium初期導入

---
+++ src/test/java/jp/co/ixui/tamura/test/feature/LoginTest.java
@@ -0,0 +1,29 @@
+package jp.co.ixui.tamura.test.feature;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.Test;
+
+
+/**
+ * ログイン周りに関する自動実行テスト
+ * @author t-kawasaki
+ *
+ */
+public class LoginTest extends FluentTest {
+
+	@Test
+    public void ログイン情報を入力してログインする() {
+
+		//ログイン画面を表示
+        goTo("https://meeting-room-booking.herokuapp.com/login");
+
+        fill("#empNo").with("1");
+        fill("#pass").with("12345678");
+        findFirst(".btn").click();
+
+        assertThat(find(".header-right a").getText()).contains("ログアウト");
+    }
+
+}
