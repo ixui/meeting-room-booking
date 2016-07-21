dca24e79397b468b3baeac64c24539d78968873c
t-kawasaki <t-kawasaki@yc1.jp>
2016-07-21 17:59:17 +0900

    カレンダーに予約件数を表示

--- src/main/resources/static/js/refer-all.js
+++ src/main/resources/static/js/refer-all.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('a.calendar').click(function(){
-		var date = $(this).children().attr('value');
+		var date = $(this).children('input').attr('value');
 		document.getElementById('calendarDate').value=date;
 		document.refer.submit();
 		return false;
