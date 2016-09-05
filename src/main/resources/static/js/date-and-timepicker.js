7e01ef686d3f14f18b1511d9e026b4d34f16483c
tamura-shunsuke <f09h0224@yahoo.co.jp>
2016-09-05 12:45:53 +0900

    enhancement#127
    timepicker追加。

--- src/main/resources/static/js/date-and-timepicker.js
+++ src/main/resources/static/js/date-and-timepicker.js
@@ -1,6 +1,12 @@
-$( function() {
+$(function() {
 	var dateFormat = 'yy-mm-dd'
 	$( "#rsvDate" ).datepicker({
 		dateFormat: dateFormat
 	});
+});
+
+$(function() {
+    var options = { step: 30, timeFormat: 'Hi' };
+    $('#startTime').timepicker(options);
+    $('#endTime').timepicker(options);
 });
