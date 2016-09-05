f22fd528547743b5860d18f0ef145c1f70b2229a
tamura-shunsuke <f09h0224@yahoo.co.jp>
2016-09-05 14:54:19 +0900

    enhacement#130
    時間としてふさわしくない数字を登録させないためのバリデータを作成

---
+++ src/main/java/jp/co/ixui/tamura/controller/reservation/validator/annotation/Time.java
@@ -0,0 +1,39 @@
+package jp.co.ixui.tamura.controller.reservation.validator.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+
+import jp.co.ixui.tamura.controller.reservation.validator.TimeValidator;
+
+/**
+ * @author tamura
+ *
+ */
+@Documented
+@Constraint(validatedBy = TimeValidator.class)
+@Target({ ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+@ReportAsSingleViolation
+@SuppressWarnings("javadoc")
+public @interface Time {
+
+	String message() default "時間として正しい数字を入力してください";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	@Target({ ElementType.TYPE, ElementType.FIELD })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    public @interface List {
+        Time[] value();
+	}
+}
