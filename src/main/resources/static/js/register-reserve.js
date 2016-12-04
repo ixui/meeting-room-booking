f9fb57f6a70dad031a3657e9856fd70df9ee355d
tamura-shunsuke <f09h0224@yahoo.co.jp>
2016-12-05 08:56:13 +0900

    #153
    新規予約画面で戻るボタンを押したらカレンダー画面に戻るようにする。

--- src/main/resources/static/js/register-reserve.js
+++ src/main/resources/static/js/register-reserve.js
@@ -14,21 +14,24 @@ $(function() {
 	$('.return').click(function() {
 		$('.btn').attr('disabled', true);
 
-		var ref = document.referrer;
+//		var ref = document.referrer;
+//
+//		if ("" == ref) {
+//			window.location = '/calendar';
+//			return;
+//		}
+//
+//		if (ref == 'https://meeting-room-booking.herokuapp.com/reservationList'){
+//			var rsvDate = $('#rsvDate').attr('value');
+//			var calendarDate = rsvDate.replace(/-/g, '');
+//			$('#calendarDate').attr('value', calendarDate);
+//			$('#returnForm').submit();
+//			return;
+//		}
+//
+//		window.location = ref;
 
-		if ("" == ref) {
-			window.location = '/calendar';
-			return;
-		}
-
-		if (ref == 'https://meeting-room-booking.herokuapp.com/reservationList'){
-			var rsvDate = $('#rsvDate').attr('value');
-			var calendarDate = rsvDate.replace(/-/g, '');
-			$('#calendarDate').attr('value', calendarDate);
-			$('#returnForm').submit();
-			return;
-		}
-
-		window.location = ref;
+		// 戻るボタン押下時カレンダー画面へ遷移させる
+		window.location = '/calendar';
 	});
 });
