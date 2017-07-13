36c6d52f835947c7f054a978854d3a31b652e0c8
tamura-shunsuke <f09h0224@yahoo.co.jp>
2017-07-13 13:42:40 +0900

    #167
    Element-ui　導入

--- frontend/src/components/Admin.vue
+++ frontend/src/components/Admin.vue
@@ -7,19 +7,22 @@
     <div id="resultTable">
       <div v-show="users != null" class="table-responsive col-sm-offset-3 col-sm-6" align="center">
         <form action="/admin/delete" method="post" id="userDeleteForm">
-          <table class="table table-bordered table table-striped">
-            <tr>
-              <th class="col-sm-1"></th><th class="col-sm-3">社員番号</th><th class="col-sm-3">名前</th><th class="col-sm-3">権限</th><th class="col-sm-1"></th>
-            </tr>
-            <tr v-for="user in users">
-              <td><input type="checkbox" class="form-control input-sm" name="empNo" v-model="user.empNo" /></td>
-              <td>{{user.empNo}}</td>
-              <td>{{user.name}}</td>
-              <td v-show="user.auth == 'ROLE_USER'">ユーザ</td>
-              <td v-show="user.auth == 'ROLE_ADMIN'">管理者</td>
-              <td><button type="button" class="btn btn-default btn-sm active select">選択</button></td>
-            </tr>
-          </table>
+          <el-table ref="multipleTable" :data="users" border style="width: 50%">
+            <el-table-column type="selection" width="50">
+            </el-table-column>
+            <el-table-column sortable prop="empNo" label="社員番号">
+            </el-table-column>
+            <el-table-column prop="name" label="名前">
+            </el-table-column>
+            <el-table-column prop="auth" label="権限">
+            </el-table-column>
+            <el-table-column label="">
+              <template scope="scope">
+                <el-button size="small" @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+                <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </form>
         <button type="button" class="btn btn-default btn-sm active" id="register">新規登録</button>
         <button type="button" class="btn btn-default btn-sm active" id="delete">削除</button>
@@ -47,7 +50,6 @@ export default {
     // 作成時にユーザーリストを取得
     this.$store.dispatch('user/load').then(() => {
       // this.loading = false
-      alert('取得完了')
     }).catch(e => {
       console.log(e)
     })
@@ -55,27 +57,37 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 
 a {
   color: #42b983;
 }
 
+table {
+	min-width: 650px;
+}
+
 th, td {
   font-size: 16px
+}
+
+th:nth-child(1),
+td:nth-child(1) {
+	min-width: 50px;
+	width: 10%;
+}
+th:nth-child(2),
+td:nth-child(2),
+th:nth-child(3),
+td:nth-child(3),
+th:nth-child(4),
+td:nth-child(4) {
+	min-width: 120px;
+	width: 20%;
+}
+th:nth-child(5),
+td:nth-child(5) {
+	min-width: 200px;
+	width: 30%;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-default/index.css'
 import Admin from '../components/Admin'
 
+Vue.use(ElementUI, {locale})
 Vue.use(Router)
 
 export default new Router({
